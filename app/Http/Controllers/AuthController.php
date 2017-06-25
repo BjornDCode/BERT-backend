6a3bec7a82c9ca1e3a02a536982b60c4b049f419
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-06-25 23:10:45 +0200

    Auth routes working

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use JWTAuth;
+use Carbon\Carbon;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterFormRequest;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
 
-    public function signup() {
-        //
+    public function signup(RegisterFormRequest $request) {
+        User::create([
+            'username' => $request->json('username'),
+            'email' => $request->json('email'),
+            'password' => bcrypt($request->json('password')),
+        ]);
     }
 
-    public function signin() {
-        // 
+    public function signin(Request $request) {
+
+        try {
+            $token = JWTAuth::attempt($request->only('email', 'password'), [
+                'exp' => Carbon::now()->addWeek()->timestamp,
+            ]);
+        } catch (JWTException $e) {
+            return response()->json([
+                'error' => 'Could not authenticate'
+            ], 500);
+        }
+
+        if (!$token) {
+            return response()->json([
+                'error' => 'Could not authenticate'
+            ], 401);
+        }
+
+        return response()->json(compact('token'));
+
     }
 
 }
