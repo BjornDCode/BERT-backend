68a744dcaf1ff14942b5da8fe74d67ffcc522ad8
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-06-25 22:31:11 +0200

    Fractal added

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Spatie\Fractal\FractalServiceProvider::class,
 
         /*
          * Application Service Providers...
