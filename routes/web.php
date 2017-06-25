cff4c85dc284d8736dddf2d036cc3df43d5c7cde
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-06-25 22:27:45 +0200

    Initial routes, settings and jwt added

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::group(['middleware' => ['api']], function() {
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::post('/auth/signup', [
+        'uses' => 'AuthController@signup'
+    ]);
+
+    Route::post('auth/signin', [
+        'uses' => 'AuthController@signin'
+    ]);
+
 });
